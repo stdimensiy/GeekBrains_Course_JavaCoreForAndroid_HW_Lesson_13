fd9c4c41529f371c166bf00d4d84bc4e615805a1
StDimensiy <stdimensiy@yandex.ru>
2021-02-18 17:53:42 +0300

    Базовое задание выполнено

--- src/main/java/ru/geekbrains/JavaCoreForAndroid/Lesson13.java
+++ src/main/java/ru/geekbrains/JavaCoreForAndroid/Lesson13.java
@@ -1,5 +1,6 @@
 package ru.geekbrains.JavaCoreForAndroid;
 
+import java.util.concurrent.CountDownLatch;
 /**
  * Сourse: java core for android
  * Faculty of Geek University Android Development
@@ -8,11 +9,34 @@ package ru.geekbrains.JavaCoreForAndroid;
  * Group 24.12.2020
  * <p>
  * HomeWork for lesson 13
- * Created 15.02.2021
+ * Created 18.02.2021
  * v 1.0
  */
 public class Lesson13 {
-    public static void main(String[] args) {
-        
+    public static final int CARS_COUNT = 4;
+    static final CountDownLatch cdlStart = new CountDownLatch(1);
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
+        Race race = new Race(new Road(60), new Tunnel(), new Road(40));
+        Car[] cars = new Car[CARS_COUNT];
+        Thread[] threads = new Thread[CARS_COUNT];
+        for (int i = 0; i < cars.length; i++) {
+            cars[i] = new Car(race, 20 + (int) (Math.random() * 10));
+        }
+        for (int i = 0; i < cars.length; i++) {
+            threads[i] = new Thread(cars[i]);
+            threads[i].start();
+        }
+        try {
+            cdlStart.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+              System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+        for (int i = 0; i < cars.length; i++) {
+            threads[i].join();
+        }
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
     }
 }
+
